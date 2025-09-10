b659daae970af525c9442a4f17b3d2de4b465310
Dominik Gronkiewicz <gronki@gmail.com>
2025-09-10 18:15:17 +0200

    download test data with cmake

--- get-test-data.sh
+++ get-test-data.sh
@@ -1,1 +1,3 @@
-curl -L https://github.com/arstgit/high-frequency-vocabulary/raw/refs/heads/master/30k.txt -o english30k.txt
+if [ ! -f english30k.txt ]; then
+	curl -L https://github.com/arstgit/high-frequency-vocabulary/raw/refs/heads/master/30k.txt -o english30k.txt
+fi
